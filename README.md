3b27a655a5c3e8af825def2752775d2cc1f167c2
Hugo Coto <hugocoto100305@gmail.com>
2026-06-28 13:44:34 +0200

    Adding images

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gogh nvim theme
+
+![palette](images/Screenshot-2026-06-28_13:41:21.png)
+![image](images/Screenshot-2026-06-28_13:43:49.png)
--- lua/gogh/palette.lua
+++ lua/gogh/palette.lua
@@ -1,15 +1,15 @@
 return {
-    -- bg        = "#272727",
-    bg        = "NONE",    -- Bg
-    bg_alt    = "#141617", -- Bar, Menu, ...
-    fg        = "#ddc7a1", -- Text (fg)
-    muted     = "#a89984", -- Comments, line numbers, dimmed text
+    bg      = "NONE", -- transparent bg
+    bg_alt  = "#141617",
+    fg      = "#ddc7a1",
+    muted   = "#a89984",
 
-    blue      = "#7daea3", -- Functions
-    cyan      = "#89b482", -- None
-    green     = "#a9b665", -- Strings
-    yellow    = "#d8a657", -- Warnings
-    orange    = "#e78a4e", -- Types
-    red       = "#ea6962", -- Errors, Constants
-    magenta   = "#d3869b", -- Kwords
+    blue    = "#7daea3",
+    cyan    = "#89b482",
+    green   = "#a9b665",
+    yellow  = "#d8a657",
+    orange  = "#e78a4e",
+    red     = "#ea6962",
+    magenta = "#d3869b",
 }
+
